e73e607639e6b3ce586a8ff72d5efe82ce3c8b87
Myles Gordon <myles.gordon@sky.uk>
2023-01-30 15:16:01 +0000

    Backend: trip up CI/CD

--- backend/main_test.go
+++ backend/main_test.go
@@ -2,8 +2,10 @@ package main
 
 import "testing"
 
-func TestDummy(t *testing.T) { test1 := 1
+func TestDummy(t *testing.T) {
+	test1 := 1
 	test2 := 2
+	test3 := 4
 	if test1 == test2 {
 		t.Errorf("Uhh?")
 	}
