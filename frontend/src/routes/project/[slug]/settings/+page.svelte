a9c1974ac0b83e6ae5d861a0bb45c83886ab80cf
Myles Gordon <myles.gordon@sky.uk>
2023-02-06 13:58:09 +0000

    frontend: project settings page

--- frontend/src/routes/project/[slug]/settings/+page.svelte
+++ frontend/src/routes/project/[slug]/settings/+page.svelte
@@ -1,7 +1,72 @@
 <script lang="ts">
+	import { Button, ButtonGroup, Dialog, Input } from 'agnostic-svelte';
+	import Meta from '$lib/components/Meta.svelte';
+	import VisibilityInput from '$lib/components/VisibilityInput.svelte';
+
 	import type { PageData } from "./$types";
-
-
+	import type { SvelteComponent } from 'svelte';
     export let data: PageData;
-    $: console.log(data);
+
+    let name = data.projectSettings.name; 
+    let description = data.projectSettings.description; 
+    let visibility = data.projectSettings.visibility;
+    let collaborators = data.projectSettings.collaborators;
+    let environments = data.projectSettings.environments;
+
+    let collaboratorText = collaborators.join(',');
+    let environmentText = environments.join(',');
+
+    $: collaborators = splitAndTrim(collaboratorText);
+    $: environments = splitAndTrim(environmentText);
+
+    const headingAndTitle = `${name} settings`
+
+    function splitAndTrim(input: string): Array<string> {
+        return input.split(',').map((item) => item.trim());
+    }
+
+    function onSubmit(e: SubmitEvent) {
+		console.log(typeof e);
+	}
+
+    // Delete project dialogue
+    let dialogInstance: SvelteComponent;
+    function assignDialogInstance (event: CustomEvent<SvelteComponent>) {
+        dialogInstance = event.detail.instance;
+    };
+    function openDialog() {
+        if (dialogInstance) {
+          dialogInstance.show();
+        }
+    };
+
+    function deleteProject(e: SubmitEvent) {
+		console.log(typeof e);
+	}
 </script>
+
+<Meta title={headingAndTitle} />
+<h1>{headingAndTitle}</h1>
+
+<form class="flex flex-col space-y-3 pt-4" on:submit|preventDefault={onSubmit}>
+	<Input isRounded id="name" label="Name" bind:value={name} />
+	<Input isRounded id="description" label="Description" bind:value={description} />
+    <VisibilityInput bind:visibility={visibility} />
+    <Input isRounded id="collaborators" label="Collaborators" bind:value={collaboratorText} />
+    <Input isRounded id="environments" label="Environments" bind:value={environmentText} />
+
+    <div class="flex flex-row gap-2 justify-center">
+	    <Button isBordered isRounded mode="primary" type="submit">Save</Button>
+	    <Button isBordered isRounded mode="secondary" on:click={openDialog}>Delete</Button>
+    </div>
+</form>
+
+<Dialog id="a11y-dialog" title="Delete project" dialogRoot="#dialog-root" closeButtonLabel="My close button label" closeButtonPosition="last" titleId="uniqueTitleId" role="dialog" classNames={{ title: 'h4 mbe18 flex justify-center' }} isAnimationFadeIn on:instance={assignDialogInstance} >
+    <div class="flex flex-col items-center gap-2">
+        <h2>Are you sure you want to delete {data.projectSettings.name}?</h2>
+        <form on:submit|preventDefault={deleteProject}>
+	     <Button isBordered isRounded mode="secondary" type="submit">Yes</Button>
+	     <Button isBordered isRounded mode="primary">No</Button>
+        </form>
+    </div>
+</Dialog>
--- frontend/src/routes/project/create/+page.svelte
+++ frontend/src/routes/project/create/+page.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-	import { Visibility, VisibilityOptions } from '$lib/types';
+	import { Visibility } from '$lib/types';
 	import Meta from '$lib/components/Meta.svelte';
-	import { Button, ChoiceInput, Input } from 'agnostic-svelte';
+	import VisibilityInput from '$lib/components/VisibilityInput.svelte';
+	import { Button, Input } from 'agnostic-svelte';
 	let name: string;
 	let description: string;
-	let visibility: Visibility = Visibility.Public;
+	let visibility: Visibility = Visibility.Private;
 
 	function onSubmit(e: SubmitEvent) {
 		console.log(typeof e);
 	}
-
-	let visibilityCheckedOption: Array<string> = ['Public'];
-	$: visibility = Visibility[visibilityCheckedOption[0] as Visibility];
 </script>
 
 <Meta title="Create Project" />
@@ -20,14 +18,7 @@
 <form class="flex flex-col space-y-3 pt-4" on:submit|preventDefault={onSubmit}>
 	<Input isRounded id="name" label="Name" bind:value={name} />
 	<Input isRounded id="description" label="Description" type="textarea" bind:value={description} />
-	<ChoiceInput
-		id="visibility"
-		type="radio"
-		options={VisibilityOptions}
-		legendLabel="Visibility"
-		checkedOptions={visibilityCheckedOption}
-		bind:checked={visibilityCheckedOption}
-	/>
+	<VisibilityInput bind:visibility={visibility} />
 
 	<Button isBordered isRounded mode="primary" type="submit">Create</Button>
 </form>
