8f5f4841ddd00382489be4cd6273e7da3b901aa5
Myles Gordon <myles.gordon@sky.uk>
2023-02-24 18:30:11 +0000

    frontend: add isLoggedIn unit test

--- frontend/src/test/components/Header.test.ts
+++ frontend/src/test/components/Header.test.ts
@@ -4,13 +4,15 @@ import { testMatchingSnapshot } from '../common';
 import Header from '$lib/components/Header.svelte';
 
 describe('Header component', () => {
-	let isLoggedInValue: boolean; 
-	isLoggedIn.subscribe((value) => { isLoggedInValue = value; });
+	let isLoggedInValue: boolean;
+	isLoggedIn.subscribe((value) => {
+		isLoggedInValue = value;
+	});
 
 	testMatchingSnapshot(Header, { isLoggedInResponse: false });
 	testMatchingSnapshot(Header, { isLoggedInResponse: true });
 
-	it("should update isLoggedIn store with correct value", () => {
+	it('should update isLoggedIn store with correct value', () => {
 		const testValues = [false, true];
 		testValues.forEach((testValue) => {
 			render(Header, { isLoggedInResponse: testValue });
