686b3035fa17a067e9eb14c95245fd85ec68affd
Myles Gordon <myles.gordon@sky.uk>
2023-02-24 13:45:14 +0000

    frontend: update header unit test

--- frontend/src/test/components/Header.test.ts
+++ frontend/src/test/components/Header.test.ts
@@ -1,6 +1,20 @@
+import { isLoggedIn } from '$lib/stores';
+import { render } from '@testing-library/svelte';
 import { testMatchingSnapshot } from '../common';
 import Header from '$lib/components/Header.svelte';
 
 describe('Header component', () => {
-	testMatchingSnapshot(Header);
+	let isLoggedInValue: boolean; 
+	isLoggedIn.subscribe((value) => { isLoggedInValue = value; });
+
+	testMatchingSnapshot(Header, { isLoggedInResponse: false });
+	testMatchingSnapshot(Header, { isLoggedInResponse: true });
+
+	it("should update isLoggedIn store with correct value", () => {
+		const testValues = [false, true];
+		testValues.forEach((testValue) => {
+			render(Header, { isLoggedInResponse: testValue });
+			expect(isLoggedInValue).toBe(testValue);
+		});
+	});
 });
